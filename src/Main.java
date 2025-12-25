c7420a748098a8862ad6dc159a65a7055900d4ad
xxxJOKERxxx <0888200@gmail.com>
2025-12-26 00:01:43 +0300

    массив strArray

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[10];
+        String[] strArray = new String[10];
         for (int number: array) {
             System.out.println(number);
+        }
+        for (String str: strArray) {
+            System.out.println(str);
+            
         }
 
     }
