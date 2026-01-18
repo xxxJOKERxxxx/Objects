68fa67bd954c060e421b6c0000db105c41950d0f
xxxJOKERxxx <0888200@gmail.com>
2026-01-18 13:50:38 +0300

    ДВУМЕРНЫЙ МАССИВ

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[][] array = new int[5][5];
+        int[][] array = new int [3][3];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+            for (int j =0; j <array[i].length; j++) {
+                array[i][j] = i;
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
+
         }
-
-
     }
 }
