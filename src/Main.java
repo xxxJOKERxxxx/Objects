070c9488e2128dff0896146f3f965e0f340bab2d
xxxJOKERxxx <0888200@gmail.com>
2026-02-02 22:43:36 +0300

    создвл 2 пакета, 3 класса

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Person();
         Person person2 = new Person("Andrei");
 
 
---
+++ src/packet2/AccessLevels.java
@@ -0,0 +1,4 @@
+package packet2;
+
+public class AccessLevels {
+}
